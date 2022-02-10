e59922e97d88aeb5baddcdb3cd35d865b2828619
Yeong joon <bluehome8626@naver.com>
2022-02-10 10:28:12 +0900

    ⚡:: Velopert Context API create Head

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { createGlobalStyle } from "styled-components";
+import Head from "./components/todoHead/Head";
 import Template from "./components/todoTemplate/Template";
 
 const GlobalStyle = createGlobalStyle`
@@ -12,7 +13,9 @@ function App() {
   return (
     <>
       <GlobalStyle />
-      <Template>안녕하세요</Template>
+      <Template>
+        <Head />
+      </Template>
     </>
   );
 }
